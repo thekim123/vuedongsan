0055dbc1274389351b5a7d481e0412bc56b2c146
thekim123 <tacdo12@naver.com>
2023-04-11 12:34:29 +0900

    여러가지 정렬 숙제 완료

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-
-  <ModalCard @closeModal="모달창열렸니=false" :모달창열렸니="모달창열렸니" :원룸들="원룸들" :누른거="누른거"></ModalCard>
+  <Transition name="fade">
+    <ModalCard @closeModal="모달창열렸니=false" 
+    :모달창열렸니="모달창열렸니" 
+    :원룸들="원룸들" :누른거="누른거"/>
+  </Transition>
 
   <div class="menu">
     <a v-for="(a, i) in 메뉴들" :key="i">{{a}}</a>
   </div>
+
+  <button @click="titleSort">이름순</button>
+  <button @click="priceSortAsc">가격순</button>
+  <button @click="priceSortDesc">가격역순</button>
+  <button @click="belowFifty">50만원이하</button>
+  <button @click="sortBack">되돌리기</button>
 
   <ProductCard @openModal="모달창열렸니=true; 누른거=$event" v-for="(oneRoom, i) in 원룸들" :key="oneRoom" :oneRoom="원룸들[i]" :모달창열렸니="모달창열렸니" :누른거="누른거"/>
 </template>
@@ -20,6 +29,7 @@ export default {
   name: 'App',
   data(){
     return{
+      원룸들오리지널 : [...작명],
       원룸들 : 작명,
       모달창열렸니 : false,
       스타일 : 'color : red',
@@ -31,8 +41,44 @@ export default {
   methods: {
     increase(i){
       this.신고수[i]++;
-    }
+    },
+    sortBack(){
+      this.원룸들 = [...this.원룸들오리지널];      
+    },
+    priceSortAsc(){
+      this.원룸들.sort(function(a, b){
+          return a.price-b.price;
+      });
+    },
+    priceSortDesc(){
+      this.원룸들.sort(function(a, b){
+          return b.price-a.price;
+      })
+    },
+    titleSort(){
+      this.원룸들.sort(function(a, b){        
+        if(a.title<b.title){
+          return -1;
+        } 
+        
+        if(a.title>b.title){
+          return 1;
+        }
+        
+        else{
+          return 0;
+        }
+      })
+    },
+    belowFifty(){
+      const newArr= [];
+      this.원룸들.forEach((oneRoom)=>{
+        if(oneRoom.price<=500000) newArr.push(oneRoom);
+      })
+      this.원룸들= newArr;
+    },
   },
+
 
   components: {
     ModalCard: ModalCard,
@@ -85,6 +131,44 @@ div{
   background: white;
   border-radius: 8px;
   padding: 20px;
+}
+
+/* .start{
+  opacity: 0;
+  transition: all 1s;
+}
+
+.end{
+  opacity: 1;
+  transition: all 1s;
+} */
+
+.fade-enter-from {
+  /* opacity: 0; */
+  transform: translateY(-1000px);
+}
+
+.fade-enter-active{
+  transition: all 1s;
+}
+
+.fade-enter-to{
+  /* opacity: 1; */
+  transform: translateY(0px);
+}
+
+.fade-leave-from {
+  /* opacity: 1; */
+  transform: translateY(0px);
+}
+
+.fade-leave-active{
+  transition: all 1s;
+}
+
+.fade-leave-to{
+  /* opacity: 0; */
+  transform: translateY(-1000px);
 }
 
 </style>
--- src/components/ModalCard.vue
+++ src/components/ModalCard.vue
@@ -4,7 +4,7 @@
       <img :src="원룸들[누른거].image" class="room-img">
       <h4>{{원룸들[누른거].title}}</h4>
       <p>{{원룸들[누른거].content}}</p>
-      <input v-model="month" @blur="checkInput">
+      <input v-model="month">
       <p> {{ month }} 개월 선택함 : {{ Number(원룸들[누른거].price) * Number(month) }} 원</p>
       <button @click="send();">닫기</button>
     </div>
@@ -28,16 +28,16 @@ export default {
       send(){
         this.$emit('closeModal');
       },
-      checkInput() {
-        console.log(this.month);
-        if (/^[0-9]+$/.test(this.month)) {
-          console.log('입력값이 숫자로만 이루어져 있습니다.');
-        } else {
-          const invalidChars = this.month.replace(/[0-9]/g, '');
-          alert(`숫자 외의 문자 ${invalidChars}가 포함되어 있습니다. 숫자로만 입력해주세요!`);
-          this.month = '1';
-        }
-      }
+      // checkInput() {
+      //   console.log(this.month);
+      //   if (/^[0-9]+$/.test(this.month)) {
+      //     console.log('입력값이 숫자로만 이루어져 있습니다.');
+      //   } else {
+      //     const invalidChars = this.month.replace(/[0-9]/g, '');
+      //     alert(`숫자 외의 문자 ${invalidChars}가 포함되어 있습니다. 숫자로만 입력해주세요!`);
+      //     this.month = '1';
+      //   }
+      // }
     },
     watch: {
       month(a){
